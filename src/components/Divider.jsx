8c429ee53b751c67695b1ca24a3ab9ca6e785096
Ram Gopal Reddy <redabothularamgopalreddy@gmail.com>
2025-06-21 09:00:23 +0530

    Update Divider.jsx

--- src/components/Divider.jsx
+++ src/components/Divider.jsx
@@ -1,7 +1,7 @@
 // components/Divider.jsx
 const Divider = ({ text = "" }) => {
   return (
-    <div className="flex items-center my-8">
+    <div className="flex items-center my-8 bg-black">
       <div className="flex-grow border-t border-gray-300 dark:border-gray-600"></div>
       {text && (
         <span className="mx-4 text-gray-500 dark:text-gray-400 font-medium whitespace-nowrap">
